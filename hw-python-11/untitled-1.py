57922118790ad90dddefa8955ca54f3961775f61
Mary <Mary>
2017-02-12 16:04:21 +0300

    homework added

--- hw-python-11/untitled-1.py
+++ hw-python-11/untitled-1.py
@@ -7,9 +7,16 @@ def open_file():
 
 def find_and_replace():
     text = open_file()
-    reg = re.sub(u'\b[Вв]икинг(и|у|а(м|ми|х)?|о[мв]|е)?\b', u'\\1', text)
+    reg = re.sub(r'викинг((и|а|ам|ами|ах|у|ов|ом|е)?)([\s,.!\?:"\(\)\'»])', r'бурундук\1\3', text)
+    reg = re.sub(r'Викинг((и|а|ам|ами|ах|у|ов|ом|е)?)([\s,.!\?:"\(\)\'»])', r'Бурундук\1\3', reg)
     return reg
 
-file = 'vikings.htm'
-print(find_and_replace())
+def print_text():
+    text = find_and_replace()
+    with open ('chipmunks.txt', 'w', encoding = 'utf-8') as f:
+        f.write(text)
+    return text
+
+file = 'vikings.txt'
+print(print_text())
     
